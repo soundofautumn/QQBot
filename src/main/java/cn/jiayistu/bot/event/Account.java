03a0131e43f997758a8d57b57cf909db19455078
qjm1412 <qjm1412@163.com>
2020-09-15 23:40:49 +0800

    Create Account.java

---
+++ src/main/java/cn/jiayistu/bot/event/Account.java
@@ -0,0 +1,60 @@
+package cn.jiayistu.bot.event;
+
+import cn.jiayistu.utils.DBUtil;
+import kotlin.coroutines.CoroutineContext;
+import net.mamoe.mirai.event.EventHandler;
+import net.mamoe.mirai.event.Listener;
+import net.mamoe.mirai.event.ListeningStatus;
+import net.mamoe.mirai.event.SimpleListenerHost;
+import net.mamoe.mirai.message.MessageEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Account extends SimpleListenerHost {
+    private long qq;
+
+    public Account(long id) {
+        qq = id;
+    }
+
+    public static boolean isBind(long qq) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtil.getConnection();
+
+            String sql = "SELECT qq FROM users WHERE qq = ? ";
+            ps = conn.prepareStatement(sql);
+            ps.setLong(1, qq);
+            rs = ps.executeQuery();
+            rs.next();
+            int i = rs.getInt(1);
+            return i != 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            DBUtil.close(conn, ps, rs);
+        }
+    }
+
+    @EventHandler(priority = Listener.EventPriority.HIGH)
+    public static ListeningStatus binding(MessageEvent event) {
+        event.getSubject().sendMessage("请输入您的学号");
+        //TODO 判断是否被绑定&&绑定
+
+
+        return ListeningStatus.STOPPED;
+    }
+
+    @Override
+    public void handleException(@NotNull CoroutineContext context, @NotNull Throwable exception) {
+        throw new RuntimeException("在事件处理过程中出现异常", exception);
+    }
+}
