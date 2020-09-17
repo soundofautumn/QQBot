a5ab16e4209c015d23c4324ece6baeafabf99a4e
qjm1412 <qjm1412@163.com>
2020-09-17 23:35:15 +0800

    Update UpLoad.java

--- src/main/java/cn/jiayistu/bot/event/UpLoad.java
+++ src/main/java/cn/jiayistu/bot/event/UpLoad.java
@@ -1,6 +1,6 @@
 package cn.jiayistu.bot.event;
 
-import cn.jiayistu.utils.DBUtil;
+import cn.jiayistu.utils.DataBaseUtils;
 import com.alibaba.fastjson.JSONObject;
 import kotlin.coroutines.CoroutineContext;
 import net.mamoe.mirai.event.EventHandler;
@@ -15,9 +15,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 public class UpLoad extends SimpleListenerHost {
-
-    private JSONObject music_share;//分享的音乐链接
-    private String introduce;//对音乐的介绍
+    /**
+     * 储存音乐的分享链接
+     */
+    private JSONObject musicShare;
+    /**
+     * 储存对音乐的介绍
+     */
+    private String introduce;
     private final long QQId;
 
     public UpLoad(long id) {
@@ -26,15 +31,21 @@ public class UpLoad extends SimpleListenerHost {
 
 
     @EventHandler(concurrency = Listener.ConcurrencyKind.LOCKED, priority = Listener.EventPriority.HIGH)
-    public ListeningStatus Update(MessageEvent updateEvent) {
-        if (updateEvent.getSender().getId() != QQId) return ListeningStatus.LISTENING;
+    public ListeningStatus update(MessageEvent updateEvent) {
+        if (updateEvent.getSender().getId() != QQId) {
+            return ListeningStatus.LISTENING;
+        }
 
         //如果不是上传命令,则继续监听
-        if (!updateEvent.getMessage().contentToString().equals("上传")) return ListeningStatus.LISTENING;
+        if (!"上传".equals(updateEvent.getMessage().contentToString())) {
+            return ListeningStatus.LISTENING;
+        }
         //如果是退出指令,则停止监听
-        if (updateEvent.getMessage().contentToString().equals("退出")) return ListeningStatus.STOPPED;
+        if ("退出".equals(updateEvent.getMessage().contentToString())) {
+            return ListeningStatus.STOPPED;
+        }
         //判断歌曲信息是否完整,并发送提示信息
-        if (introduce == null && music_share == null) {
+        if (introduce == null && musicShare == null) {
             updateEvent.getSubject().sendMessage("缺少歌曲介绍或音乐链接");
             return ListeningStatus.LISTENING;
         }
@@ -44,22 +55,22 @@ public class UpLoad extends SimpleListenerHost {
         Connection conn = null;
         PreparedStatement ps = null;
         try {
-            conn = DBUtil.getConnection();
+            conn = DataBaseUtils.getConnection();
             //sql语句
             String sql = "INSERT INTO music (music_name,music_singer,introduce,music_share) VALUES (?,?,?,?)";
             ps = conn.prepareStatement(sql);
 
             //获取歌名和歌手
-            String music_name = music_share.getJSONObject("meta").getJSONObject("music").getString("title");
-            String music_singer = music_share.getJSONObject("meta").getJSONObject("music").getString("desc");
+            String musicName = musicShare.getJSONObject("meta").getJSONObject("music").getString("title");
+            String musicSinger = musicShare.getJSONObject("meta").getJSONObject("music").getString("desc");
 
-            updateEvent.getSubject().sendMessage("歌名为:" + music_name);
-            updateEvent.getSubject().sendMessage("歌手为:" + music_singer);
+            updateEvent.getSubject().sendMessage("歌名为:" + musicName);
+            updateEvent.getSubject().sendMessage("歌手为:" + musicSinger);
 
-            ps.setString(1, music_name);
-            ps.setString(2, music_singer);
+            ps.setString(1, musicName);
+            ps.setString(2, musicSinger);
             ps.setString(3, introduce);
-            ps.setString(4, music_share.toString());
+            ps.setString(4, musicShare.toString());
 
             ps.executeUpdate();
             //上传成功
@@ -68,21 +79,26 @@ public class UpLoad extends SimpleListenerHost {
             updateEvent.getSubject().sendMessage("上传过程中出错,以自动退出");
             e.printStackTrace();
         } finally {
-            DBUtil.close(conn, ps);//关闭连接
+            //关闭连接
+            DataBaseUtils.close(conn, ps);
         }
 
-
-        return ListeningStatus.STOPPED;//停止监听
+        //停止监听
+        return ListeningStatus.STOPPED;
     }
 
     @EventHandler(priority = Listener.EventPriority.HIGH)
-    public ListeningStatus OnMusicShare(MessageEvent shareEvent) {
-        if (shareEvent.getSender().getId() != QQId) return ListeningStatus.LISTENING;
+    public ListeningStatus onMusicShare(MessageEvent shareEvent) {
+
+        if (shareEvent.getSender().getId() != QQId) {
+            return ListeningStatus.LISTENING;
+        }
 
 
         //如果发送的消息不是分享链接,则保持监听
         if (shareEvent.getMessage().get(1) instanceof LightApp) {
-            music_share = JSONObject.parseObject(shareEvent.getMessage().get(1).contentToString());//如果为链接则转化为json
+            //如果为链接则转化为json
+            musicShare = JSONObject.parseObject(shareEvent.getMessage().get(1).contentToString());
 
 
         }
@@ -90,18 +106,24 @@ public class UpLoad extends SimpleListenerHost {
     }
 
     @EventHandler(priority = Listener.EventPriority.HIGH)
-    public ListeningStatus UploadMusic(MessageEvent musicEvent) {
+    public ListeningStatus uploadMusic(MessageEvent musicEvent) {
 
-        if (musicEvent.getSender().getId() != QQId) return ListeningStatus.LISTENING;
+        if (musicEvent.getSender().getId() != QQId) {
+            return ListeningStatus.LISTENING;
+        }
 
 
         //介绍输入格式为 介绍:xxx
         String musicString = musicEvent.getMessage().contentToString();
 
-        String[] strings = musicString.split(":");//分割字符串
+        //分割字符串
+        String[] strings = musicString.split(":");
         //判断是否符合要求
-        if (strings.length != 2 || !strings[0].equals("介绍")) return ListeningStatus.LISTENING;
-        introduce = strings[1];//将歌曲介绍保存
+        if (strings.length != 2 || !"介绍".equals(strings[0])) {
+            return ListeningStatus.LISTENING;
+        }
+        //将歌曲介绍保存
+        introduce = strings[1];
         return ListeningStatus.LISTENING;
     }
 
