159e69a821babcab2ff20f21a4d38df2d2267a07
qjm1412 <qjm1412@163.com>
2020-09-17 23:27:20 +0800

    Update Account.java

--- src/main/java/cn/jiayistu/bot/event/Account.java
+++ src/main/java/cn/jiayistu/bot/event/Account.java
@@ -14,6 +14,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * @author QJMing
+ */
 public class Account extends SimpleListenerHost {
     private long qq;
 
