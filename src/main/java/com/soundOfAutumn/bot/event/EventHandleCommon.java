333c45bb3eba211bb5840a64c5cd1e184d2306c5
soundofautumn <qjm1412@163.com>
2020-10-02 19:49:50 +0800

    Update EventHandleCommon.java

--- src/main/java/com/soundOfAutumn/bot/event/EventHandleCommon.java
+++ src/main/java/com/soundOfAutumn/bot/event/EventHandleCommon.java
@@ -41,47 +41,55 @@ public class EventHandleCommon extends SimpleListenerHost {
 
         //判断接收到的信息
         //判断是否发送的是绑定命令
-        if ("点歌".equals(msgString)) {
-            if (!AccountUtils.isBind(event.getSender().getId())) {
-                event.getSubject().sendMessage("QQ号未绑定,请先绑定");
-                return ListeningStatus.LISTENING;
-            }
-            //发送提示
-            event.getSubject().sendMessage("请发送歌曲介绍和歌曲链接,介绍输入格式为 \"介绍:xxx\" ,最后输入 \"上传\" 来确认");
-            event.getSubject().sendMessage("如需更改上传的内容,直接重新发送即可");
+        switch (msgString) {
 
-            //注册新事件
-            Events.registerEvents(bot, new MusicUpLoad(event.getSender().getId()));
-            //判断是否发送的绑定命令
-        } else if ("绑定".equals(msgString)) {
-            //判断是否已经绑定
-            if (AccountUtils.isBind(event.getSender().getId())) {
-                event.getSubject().sendMessage("QQ号已绑定");
-            } else {
-                //绑定
-                event.getSubject().sendMessage("准备绑定....请注意一个QQ号只能绑定一个账号");
-                event.getSubject().sendMessage("请输入您的学号");
+            case "点歌":
+                if (!AccountUtils.isBind(event.getSender().getId())) {
+                    event.getSubject().sendMessage("QQ号未绑定,请先绑定");
+                    return ListeningStatus.LISTENING;
+                }
+                //发送提示
+                event.getSubject().sendMessage("请发送歌曲介绍和歌曲链接,介绍输入格式为 \"介绍:xxx\" ,最后输入 \"上传\" 来确认");
+                event.getSubject().sendMessage("如需更改上传的内容,直接重新发送即可");
 
-                Events.registerEvents(bot, new Bind(event.getSender().getId()));
+                //注册新事件
+                Events.registerEvents(bot, new MusicUpLoad(event.getSender().getId()));
+                //判断是否发送的绑定命令
+                break;
+            case "绑定":
+                //判断是否已经绑定
+                if (AccountUtils.isBind(event.getSender().getId())) {
+                    event.getSubject().sendMessage("QQ号已绑定");
+                } else {
+                    //绑定
+                    event.getSubject().sendMessage("准备绑定....请注意一个QQ号只能绑定一个账号");
+                    event.getSubject().sendMessage("请输入您的学号");
 
-            }
-        } else if (msgString.equals("@" + event.getBot().getNick())) {
-            //显示提示信息
-            event.getSubject().sendMessage("现支持的命令如下:\n" +
-                    "1. \"点歌\" :进行上传歌曲等操作\n" +
-                    "2. \"绑定\" :进行QQ号与学号的绑定(如需解绑,请联系管理员)\n" +
-                    "3. \"显示歌曲列表\" :显示简要的歌曲信息" +
-                    "4. \"显示歌曲详细信息:X\" :X为歌曲序号,显示歌曲的简介和分享链接");
-        } else if ("显示歌曲列表".equals(msgString)) {
-            event.getSubject().sendMessage(MusicList.getBrief());
-        } else if (msgString.contains("显示歌曲详细信息") && msgString.contains(":")) {
-            String s = msgString.split(":")[1];
-            event.getSubject().sendMessage(MusicList.getIntroduce(s));
-            event.getSubject().sendMessage(MusicList.getMusicShare(s));
-        } else if (msgString.contains("点赞") && msgString.contains(":")) {
-            String musicId = msgString.split(":")[1];
-            event.getSubject().sendMessage(MusicLike.giveLike(musicId, event.getSender().getId()));
+                    Events.registerEvents(bot, new Bind(event.getSender().getId()));
 
+                }
+                break;
+
+            case "显示歌曲列表":
+                event.getSubject().sendMessage(MusicList.getBrief());
+                break;
+
+            default:
+                if (msgString.contains("显示歌曲详细信息") && msgString.contains(":")) {
+                    String s = msgString.split(":")[1];
+                    event.getSubject().sendMessage(MusicList.getIntroduce(s));
+                    event.getSubject().sendMessage(MusicList.getMusicShare(s));
+                } else if (msgString.contains("点赞") && msgString.contains(":")) {
+                    String musicId = msgString.split(":")[1];
+                    event.getSubject().sendMessage(MusicLike.giveLike(musicId, event.getSender().getId()));
+                } else {
+                    event.getSubject().sendMessage("现支持的命令如下:\n" +
+                            "1. \"点歌\" :进行上传歌曲等操作\n" +
+                            "2. \"绑定\" :进行QQ号与学号的绑定(如需解绑,请联系管理员)\n" +
+                            "3. \"显示歌曲列表\" :显示简要的歌曲信息" +
+                            "4. \"显示歌曲详细信息:X\" :X为歌曲序号,显示歌曲的简介和分享链接" +
+                            "5. \"点赞某首歌曲:X \" :X为歌曲序号,点赞某首歌曲");
+                }
         }
 
 
